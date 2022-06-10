fa0bd79b11f4991c508a46d9bb49dd76c1e5c3e1
nawiq7208 <ipsurbakti7208@gmail.com>
2022-06-10 23:43:50 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,4 +4,8 @@ import cors from "cors";
 
 const app = express ();
 
+app.use(cors());
+app.use(express.json());
+app.use(fileUpload());
+
 app.listen(5000, ()=> console.log("Server Up and Running..."));
